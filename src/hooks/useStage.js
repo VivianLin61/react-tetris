9d28e24522a92eb16c18cae75fcdcb832401dc07
vivilin <vivilin@cs.sunysb.edu>
2022-08-07 16:35:23 -0400

    implement clear rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,21 @@ import { createStage } from '../helpers'
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage())
-
+  const [rowsCleared, setRowsCleared] = useState(0)
   useEffect(() => {
+    setRowsCleared(0)
+    const clearRows = (newStage) =>
+      newStage.reduce((ack, row) => {
+        if (row.findIndex((cell) => cell[0] === 0) === -1) { //Current row needs to be cleared
+          setRowsCleared((prev) => prev + 1) //Increment rows cleared
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear'])) //Do not append current row, append new empty array to top of stage
+          return ack
+        }
+        // Current row does not need to be cleared
+        ack.push(row)
+        return ack
+      }, [])
+
     const updateStage = (prevStage) => {
       // First flush the stage
       const newStage = prevStage.map((row) =>
@@ -12,7 +25,6 @@ export const useStage = (player, resetPlayer) => {
       )
 
       //Draw the tetromino
-
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
@@ -27,6 +39,7 @@ export const useStage = (player, resetPlayer) => {
       // Check if piece collided and then generate new piece
       if (player.collided) {
         resetPlayer()
+        return clearRows(newStage)
       }
       return newStage
     }
@@ -40,5 +53,5 @@ export const useStage = (player, resetPlayer) => {
     resetPlayer,
   ])
 
-  return [stage, setStage]
+  return [stage, setStage, rowsCleared]
 }
